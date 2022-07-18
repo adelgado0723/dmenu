d944340539eea444657b3fbcec10f82a0fd76edd
adelgado0723 <adelgado0723@gmail.com>
2022-07-18 17:59:49 -0400

    allows emojis

--- config.def.h
+++ config.def.h
@@ -5,7 +5,10 @@ static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
 static int fuzzy = 1;
 static const unsigned int alpha = 0xf0;
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {"Hack:size=14"};
+static const char *fonts[] = {
+    "FantasqueSansMono Nerd Font Mono:size=14",
+    "Noto Color Emoji:size=14:antialias=true:autohint=true"};
+
 static const char *prompt =
     NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
