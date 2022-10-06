0aad249c2267b84cdfb8a8739db40d06bb77419f
adelgado <adelgado0723@gmail.com>
2022-10-06 14:23:31 -0400

    adds patch that allows setting the width

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 1;      /* -b  option; if 0, dmenu appears at bottom     */
 static int fuzzy = 1;
 static const unsigned int alpha = 0xf0;
 /* -fn option overrides fonts[0]; default X11 font or font set */
